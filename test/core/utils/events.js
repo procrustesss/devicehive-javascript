90236b0596fe3b65c09f06d02b482b4328d8a1e4
rbublyk <roman.bublik91@gmail.com>
2015-06-25 16:47:22 +0300

    Add creating Events object with new operator, for deleting all Events.prototype statements.

--- test/core/utils/events.js
+++ test/core/utils/events.js
@@ -6,75 +6,76 @@ var sinon = require('sinon');
 describe('Events tests', function(){
     vm.runInThisContext(fs.readFileSync('./src/core/utils/events.js'));
 
+    var events = new Events();
     afterEach(function(done){
-        Events.prototype.unbind();
+        events.unbind();
         done();
     });
 
     describe('bind()', function(){
         it('should bind right callback to event', function(){
             var cb = sinon.spy();
-            Events.prototype.bind('name', cb);
-            Events.prototype.trigger('name');
+            events.bind('name', cb);
+            events.trigger('name');
             expect(cb.calledOnce).to.be.true;
         });
         it('should return object with unbind method, which should unbind callback from event', function(){
             var cb = sinon.spy();
-            Events.prototype.bind('name', cb).unbind();
-            Events.prototype.trigger('name');
+            events.bind('name', cb).unbind();
+            events.trigger('name');
             expect(cb.called).to.be.false;
         });
     });
 
     describe('unbind()', function(){
         it('should throw exception when no events was bound', function(){
-            expect(function() { Events.prototype.unbind('name'); }).to.throw(TypeError);
+            expect(function() { events.unbind('name'); }).to.throw(TypeError);
         });
         it('should return object with null _handlers property', function(){
-            expect(Events.prototype.unbind()).to.have.property('_handlers', null);
+            expect(events.unbind()).to.have.property('_handlers', null);
         });
         it('should return right object, when no events for passed name was found', function(){
-            Events.prototype.bind('name', function(){});
-            var result = Events.prototype.unbind('anotherName');
+            events.bind('name', function(){});
+            var result = events.unbind('anotherName');
             expect(Object.keys(result._handlers).length).to.be.equal(1);
         });
         it('should delete all handlers for passed name, when no callback was passed as a parameter', function(){
-            Events.prototype.bind('name', function(){});
-            Events.prototype.bind('name', function(){});
-            Events.prototype.bind('anotherName', function(){});
-            var handlerKeys = Object.keys(Events.prototype.unbind('name')._handlers);
+            events.bind('name', function(){});
+            events.bind('name', function(){});
+            events.bind('anotherName', function(){});
+            var handlerKeys = Object.keys(events.unbind('name')._handlers);
             expect(handlerKeys.length).to.be.equal(1);
             expect(handlerKeys[0]).to.be.equal('anotherName');
         });
         it('should not unbind callback from event, if callbacks are not equal', function(){
             var originCb = sinon.spy();
             var remainingCb = sinon.spy();
-            Events.prototype.bind('name', originCb);
-            Events.prototype.unbind('name', remainingCb);
-            Events.prototype.trigger('name');
+            events.bind('name', originCb);
+            events.unbind('name', remainingCb);
+            events.trigger('name');
             expect(originCb.calledOnce).to.be.true;
             expect(remainingCb.called).to.be.false;
         });
         it('should delete events with the same name and callback', function(){
             var cb = sinon.spy();
-            Events.prototype.bind('name', cb);
-            var handlerKeys = Object.keys(Events.prototype.unbind('name')._handlers);
+            events.bind('name', cb);
+            var handlerKeys = Object.keys(events.unbind('name')._handlers);
             expect(handlerKeys.length).to.be.equal(0);
-            Events.prototype.trigger('name');
+            events.trigger('name');
             expect(cb.called).to.be.false;
         });
     });
 
     describe('trigger()', function(){
         it('should return object with null _handlers property', function(){
-            expect(Events.prototype.trigger('event')).to.have.property('_handlers', null);
+            expect(events.trigger('event')).to.have.property('_handlers', null);
         });
         it('should trigger right event', function(){
             var triggeredCb = sinon.spy();
             var notTriggeredCb = sinon.spy();
-            Events.prototype.bind('triggered', triggeredCb);
-            Events.prototype.bind('notTriggered', notTriggeredCb);
-            Events.prototype.trigger('triggered');
+            events.bind('triggered', triggeredCb);
+            events.bind('notTriggered', notTriggeredCb);
+            events.trigger('triggered');
             expect(triggeredCb.calledOnce).to.be.true;
             expect(notTriggeredCb.called).to.be.false;
         });
@@ -87,8 +88,8 @@ describe('Events tests', function(){
             var firstContext = { cxt: 'first' };
             var secondContext = { cxt: 'second' };
             var args = [ 1, 2 ];
-            var events = [{ context: firstContext, callback: firstCb}, { context: secondContext, callback: secondCb}];
-            Events.prototype._triggerEvents(events, args);
+            var eventsArr = [{ context: firstContext, callback: firstCb}, { context: secondContext, callback: secondCb}];
+            events._triggerEvents(eventsArr, args);
             expect(firstCb.calledOnce).to.be.true;
             expect(firstCb.alwaysCalledWith(1, 2)).to.be.true;
             expect(firstCb.alwaysCalledOn(firstContext)).to.be.true;
